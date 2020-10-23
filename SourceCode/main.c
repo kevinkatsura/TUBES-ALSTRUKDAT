a269eb783dc5033ce1dfe509471f43b599710647
kevinkatsura <kevinkatsura@users.noreply.github.com>
2020-10-23 10:36:20 +0700

    main menu

---
+++ SourceCode/main.c
@@ -0,0 +1,9 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(){
+    printf("// Welcome to Willy wangky's fum factory!!//\n") ;
+    printf("-->> New game \n");
+    printf("-->> Load game \n") ;
+    printf("-->> Exit \n\n");
+}
