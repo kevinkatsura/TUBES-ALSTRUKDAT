63c6a0ecfc3b3aeb676cd4302feb1a7a6b381a7a
epata <epatatuah180901@gmail.com>
2020-11-17 19:54:37 +0700

    nyatuin perintah2nya dulu

---
+++ src/perintah.h
@@ -0,0 +1,42 @@
+#ifndef PERINTAH_H
+#define PERINTAH_H
+
+#include "stack/stackt.h"
+#include "queue/queue.h"
+void newGame();
+
+void exit();
+
+void loadGame();
+
+void W();
+
+void A();
+
+void S();
+
+void D();
+
+void build (Stack *S);
+
+void upgrade (Stack *S);
+
+void buy (Stack *S);
+
+void undo (Stack *S);
+
+void execute (Stack *S);
+
+void main (Stack *S);
+
+void serve(Queue *Q);
+
+void repair();
+
+void detail();
+
+void office();
+
+void prepare();
+
+#endif
